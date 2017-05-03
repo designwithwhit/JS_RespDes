51430cbfb0b438e43f3e0db46cc5e0629deb557a
whitneylarene <whitney.larene@gmail.com>
2017-05-02 20:08:55 -0700

    Added randomloc to battle ship

--- week02/battleship/battleship.js
+++ week02/battleship/battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -12,7 +13,7 @@ while (isSunk == false){
 		if (guess < 0 || guess > 6){
 			alert ("You have guessed an invalid number. Please enter a number from 0-6:");
 		} else {
-		guesses = guesses + 1;	
+		guesses = guesses + 1;
 
 		if (guess == location1 || guess == location2 || guess == location3) {
 			alert("HIT!");
@@ -30,9 +31,3 @@ while (isSunk == false){
 var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
 
 alert(stats);
-
-
-
-
-
-
